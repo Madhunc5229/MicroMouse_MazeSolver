89f57aa9a4d740c775bca8fee982ecf0152b54f6
Madhunc5229 <61328094+Madhunc5229@users.noreply.github.com>
2022-01-15 12:29:39 -0500

    Delete doc directory

--- doc/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['listen_2',['listen',['../main_8cpp.html#a46f314570a64788cc65e832ea0152a9e',1,'main.cpp']]]
-];
--- doc/html/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['movebaseclient_11',['MoveBaseClient',['../main_8cpp.html#a21e20cc0b6656ae897b3cbb969b93241',1,'main.cpp']]]
-];
